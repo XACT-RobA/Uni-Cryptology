7279c6fc054d77cd2239a5837b8171f83e8bf2db
XACT-RobA <robertanderson1993@gmail.com>
2014-12-27 23:16:51 +0000

    Corrected letter_freqs

--- python/letter_freqs.py
+++ python/letter_freqs.py
@@ -1,11 +1,15 @@
 input_string = 'LGOABIAIRFRENPKEHSEGHEAIENIIMTSESNCAGTMACSNEOSDRUYNRTLYBOEYOYPTFRCHLTLTAAAAOEHLEVCGTESSEKRY'
+input_string_2 = 'V TJXCS CVRD PJX MJ MDCC ND TKDY PJX LQD CDLIVYZ BJ MKLM V HLY HLCC L MLEV'
+
 alph = list('abcdefghijklmnopqrstuvwxyz'.upper())
 
+input_string_2 = ''.join(i for i in input_string_2 if i in alph)
+
 def get_percentages(this_string):
-    letter_count = [0]*26
-    for letter in input_string:
+    letter_count = [0.0]*26
+    for letter in this_string:
         letter_count[alph.index(letter)] += 1
     for letter in alph:
-        print(letter + ': ' + str(letter_count[alph.index(letter)]/len(input_string)))
+        print(letter + ': ' + str("%.3f" % ((letter_count[alph.index(letter)]/len(this_string))*100)) + '%')
         
-get_percentages(input_string)
+get_percentages(input_string_2)
