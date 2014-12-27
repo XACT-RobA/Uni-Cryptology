dc0973ec67bb0505b3754f407d98f80a6bcda8f2
XACT-RobA <robertanderson1993@gmail.com>
2014-12-27 19:05:51 +0000

    Updated letter frequency file

--- python/letter_freqs.py
+++ python/letter_freqs.py
@@ -1,10 +1,11 @@
 input_string = 'LGOABIAIRFRENPKEHSEGHEAIENIIMTSESNCAGTMACSNEOSDRUYNRTLYBOEYOYPTFRCHLTLTAAAAOEHLEVCGTESSEKRY'
 alph = list('abcdefghijklmnopqrstuvwxyz'.upper())
 
-letter_count = [0]*26
-
-for letter in input_string:
-    letter_count[alph.index(letter)] += 1
-
-for letter in alph:
-    print(letter + ': ' + str(letter_count[alph.index(letter)]))
+def get_percentages(this_string):
+    letter_count = [0]*26
+    for letter in input_string:
+        letter_count[alph.index(letter)] += 1
+    for letter in alph:
+        print(letter + ': ' + str(letter_count[alph.index(letter)]/len(input_string)))
+        
+get_percentages(input_string)
